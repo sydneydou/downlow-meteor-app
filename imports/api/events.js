09a914a2fefb4b4eca79d0aad2be7c1e0c2c7c26
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-08-01 15:07:44 -0700

    Created bare publish functions for events and users

--- imports/api/events.js
+++ imports/api/events.js
@@ -2,3 +2,10 @@ import { Mongo } from "meteor/mongo";
 
 export const events = new Mongo.Collection("events");
 
+
+if (Meteor.isServer) {
+    // Meteor.publish('getEvents', function eventsPublication() {
+
+    //   return events.find({ owner: this.userId });
+    // });
+  }
--- imports/api/users.js
+++ imports/api/users.js
@@ -2,3 +2,9 @@ import { Mongo } from "meteor/mongo";
 
 export const users = new Mongo.Collection("users");
 
+if (Meteor.isServer) {
+    // Meteor.publish('getUsers', function usersPublication() {
+
+    //   return users.find({ owner: this.userId });
+    // });
+  }
