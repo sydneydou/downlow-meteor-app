17fbf19fe4dcfb76f3945239892ccb77c4770e17
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-08-01 13:41:16 -0700

    Added src folder with routes file

--- client/main.jsx
+++ client/main.jsx
@@ -3,6 +3,19 @@ import { Meteor } from "meteor/meteor";
 import { render } from "react-dom";
 import App from "/imports/ui/App";
 
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+
+
 Meteor.startup(() => {
   render(<App />, document.getElementById("react-target"));
 });
---
+++ client/src/routes/index.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+import { Redirect, Route, Switch } from "react-router";
+
+export default () => (
+    <Fragment>
+    
+         
+    </Fragment>
+  );
+  
