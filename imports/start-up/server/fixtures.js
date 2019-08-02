594ac255c60f21e314a6c57a012a2c0787920f0d
Andrew DiBiase <dibiase.andrew@outlook.com>
2019-08-02 11:22:13 -0700

    add fixtures and removed users.js api

---
+++ imports/start-up/server/fixtures.js
@@ -0,0 +1,25 @@
+import { Meteor } from "meteor/meteor";
+import { Events } from "../../api/events";
+import { Accounts } from "meteor/accounts-base";
+
+Meteor.startup(() => {
+  if (Meteor.users.find().count() === 0) {
+    user = Accounts.createUser({
+      email: "animal@party.com",
+      password: "password",
+      username: "Partyanimal"
+    });
+  }
+
+  if (Events.find().count() === 0) {
+    Events.insert({
+      title: "Shambhala",
+      artist: "Skrillex",
+      date: "Aug.9th, 9:00p.m",
+      genre: "Dub Step",
+      location: "9999 Granville St",
+      reserved: [],
+      createdBy: []
+    });
+  }
+});
