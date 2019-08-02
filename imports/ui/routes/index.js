a1eccbc930379004f6566fd6070c447cd8c84700
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-08-02 15:21:30 -0700

    User logs in and is redirected to home page

--- imports/ui/routes/index.js
+++ imports/ui/routes/index.js
@@ -4,31 +4,39 @@ import HomePage from "../pages/HomePage";
 import CreateEventPage from "../pages/CreateEventPage";
 import ProfilePage from "../pages/ProfilePage";
 import LoginPage from "../pages/LoginPage";
-import Header from "../components/Header";
-// const isLoggedIn = !!currentUser 
-export default () => {
-   
+import { withTracker } from "meteor/react-meteor-data";
+import { Meteor } from "meteor/meteor";
 
-    // if (isLoggedIn) {
-    //     return (
-    //       <Switch>
-    //         <Route path="/welcome" component={HomeContainer} />
-    //         <Redirect from="*" to="/welcome" />
-    //       </Switch>
-    //     );
-    //   }
+Routes = () => {
+  const currentUser = Meteor.user();
+  console.log(currentUser);
+  const isLoggedIn = !!currentUser;
+  if (isLoggedIn) {
+    return (
+      <Switch>
+        <Route path="/home" component={HomePage} />
+        <Redirect from="*" to="/home" />
+      </Switch>
+    );
+  }
 
-      return (
+  return (
+    <Fragment>
+      <Switch>
+        <Route path="/home" component={HomePage} />
+        <Route path="/create" component={CreateEventPage} exact />
+        <Route path="/profile" component={ProfilePage} exact />
+        <Route path="/welcome" component={LoginPage} exact />
+        <Redirect path="*" to="/home" />
+      </Switch>
+    </Fragment>
+  );
+};
 
-  <Fragment>
-    <Header/>
-    <Switch>
-      <Route path="/home" component={HomePage} />
-      <Route path="/create" component={CreateEventPage} exact />
-      <Route path="/profile" component={ProfilePage} exact />
-      <Route path="/welcome" component={LoginPage} exact />
-      <Redirect path="*" to="/home" />
-    </Switch>
-  </Fragment>
-      )
-    };
+export default withTracker(() => {
+  // Meteor.subscribe('myTodos'); // NEW!
+  return {
+    currentUser: Meteor.user()
+    // currentUserId:Meteor.user
+  };
+})(Routes);
