7bff111baee6062e61db549086d9082881680433
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-08-01 17:35:21 -0700

    Login account wrapper added to welcome page

--- imports/ui/pages/LoginPage/LoginPage.jsx
+++ imports/ui/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+import AccountsUIWrapper from "../../components/AccountWrapper"
 
 LoginPage = () => {
   return (
     <div>
+      <AccountsUIWrapper>
       <p>This is the LoginPage</p>
+      </AccountsUIWrapper>
     </div>
   );
 };
