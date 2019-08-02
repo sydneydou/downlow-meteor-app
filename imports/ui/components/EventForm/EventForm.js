ae4d7709c8644ec6688ab2b7f2bd84817ebdf17d
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-08-02 09:35:31 -0700

    Fixed router bug

--- imports/ui/components/EventForm/EventForm.js
+++ imports/ui/components/EventForm/EventForm.js
@@ -1,1 +1,115 @@
 import React from "react";
+
+// class AccountForm extends Component {
+//     constructor(props) {
+//       super(props);
+     
+//     }
+  
+//     render() {
+//       const { } = this.props;
+  
+//       return (
+//         <Form
+  
+//           validate={validate.bind(this)}
+//           render={({ handleSubmit, pristine, invalid, submitting, form }) => (
+//             <form onSubmit={handleSubmit} className={classes.accountForm}>
+//               {!this.state.formToggle && (
+//                 <FormControl fullWidth className={classes.formControl}>
+//                   <InputLabel htmlFor="fullname">Username</InputLabel>
+  
+//                   <Field name="fullname">
+//                     {({ input, meta }) => (
+//                       <Input
+//                         id="fullname"
+//                         type="text"
+//                         inputProps={{
+//                           ...input,
+//                           autoComplete: "off"
+//                         }}
+//                         value={input.value}
+//                       />
+//                     )}
+//                   </Field>
+//                 </FormControl>
+//               )}
+//               <FormControl fullWidth className={classes.formControl}>
+//                 <InputLabel htmlFor="email">Email</InputLabel>
+  
+//                 <Field name="email">
+//                   {({ input, meta }) => (
+//                     <Input
+//                       id="email"
+//                       type="text"
+//                       inputProps={{
+//                         ...input,
+//                         autoComplete: "off"
+//                       }}
+//                       value={input.value}
+//                     />
+//                   )}
+//                 </Field>
+//               </FormControl>
+//               <FormControl fullWidth className={classes.formControl}>
+//                 <InputLabel htmlFor="password">Password</InputLabel>
+  
+//                 <Field name="password">
+//                   {({ input, meta }) => (
+//                     <Input
+//                       id="password"
+//                       type="password"
+//                       inputProps={{
+//                         ...input,
+//                         autoComplete: "off"
+//                       }}
+//                       value={input.value}
+//                     />
+//                   )}
+//                 </Field>
+//               </FormControl>
+//               <FormControl className={classes.formControl}>
+//                 <Grid
+//                   container
+//                   direction="row"
+//                   justify="space-between"
+//                   alignItems="center"
+//                 >
+//                   <Button
+//                     type="submit"
+//                     className={classes.formButton}
+//                     variant="contained"
+//                     size="large"
+//                     color="secondary"
+//                     disabled={pristine || invalid}
+//                   >
+//                     {this.state.formToggle ? "Enter" : "Create Account"}
+//                   </Button>
+//                   <Typography>
+//                     <button
+//                       className={classes.formToggle}
+//                       type="button"
+//                       onClick={() => {
+//                         form.reset();
+//                         this.setState({
+//                           formToggle: !this.state.formToggle
+//                         });
+//                       }}
+//                     >
+//                       {this.state.formToggle
+//                         ? "Create an account."
+//                         : "Login to existing account."}
+//                     </button>
+//                   </Typography>
+//                 </Grid>
+//               </FormControl>
+//               <Typography className={classes.errorMessage}>
+//                 {(this.state.error &&
+//                   this.state.formToggle)}
+//               </Typography>
+//             </form>
+//           )}
+//         />
+//       );
+//     }
+//   }
--- imports/ui/pages/LoginPage/LoginPage.jsx
+++ imports/ui/pages/LoginPage/LoginPage.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import AccountsUIWrapper from "../../components/AccountWrapper"
+
 import "./styles.css";
 
 LoginPage = () => {
   return (
     <div className="login-wrapper">
-      <AccountsUIWrapper>
+      
       <p>This is the LoginPage</p>
-      </AccountsUIWrapper>
+      
     </div>
   );
 };
