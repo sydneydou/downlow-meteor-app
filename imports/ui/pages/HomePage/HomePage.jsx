5661118eb141e4519e30088a0fd490b1db4cc0e4
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-08-07 10:04:22 -0700

    Username displaying on home page

--- imports/ui/pages/HomePage/HomePage.jsx
+++ imports/ui/pages/HomePage/HomePage.jsx
@@ -8,10 +8,10 @@ class HomePage extends Component {
   render() {
     const { currentUser, classes, events } = this.props;
     return (
-      // <div>
-      //   <h1>Welcome to DownLow {currentUser.username}!</h1>
-      <EventGrid events={events} />
-      // </div>
+      <div>
+        <h1>Welcome to DownLow {currentUser.username}!</h1>
+        <EventGrid events={events} />
+      </div>
     );
   }
 }
