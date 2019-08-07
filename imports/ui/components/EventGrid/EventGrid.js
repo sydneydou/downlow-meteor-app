9e08ea50c64c9409ecdd2cb08a455f42b56fa689
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-08-07 12:57:55 -0700

    User id added to corresponding reserved array for an event when clicked

--- imports/ui/components/EventGrid/EventGrid.js
+++ imports/ui/components/EventGrid/EventGrid.js
@@ -1,15 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles";
 import EventCard from "../EventCard";
+import Button from "@material-ui/core/Button";
+import { Meteor } from "meteor/meteor";
 
-const EventGrid = ({ classes, events }) => {
-  return (
-    <Grid container direction="row" justify="space-between" spacing={5}>
-      {events && events.map(event => <EventCard event={event} />)}
-    </Grid>
-  );
-};
+class EventGrid extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showAllEvents: true
+    };
+  }
+  showAllEvents = () => {
+    this.setState({
+      showAllEvents: !this.state.showAllEvents
+    });
+  };
+
+  render() {
+    const { showAllEvents } = this.state;
+    const { classes, events } = this.props;
+    return (
+      <div>
+        {showAllEvents ? (
+          <Button
+            variant="contained"
+            onClick={() => {
+              this.showAllEvents();
+            }}
+          >
+            Events I'm Attending
+          </Button>
+        ) : (
+          <Button
+            variant="contained"
+            onClick={() => {
+              this.showAllEvents();
+            }}
+          >
+            All Events
+          </Button>
+        )}
+        {showAllEvents ? (
+          <Grid container direction="row" justify="space-between" spacing={5}>
+            {events &&
+              events.map(event => <EventCard key={event._id} event={event} />)}
+          </Grid>
+        ) : (
+          <Grid container direction="row" justify="space-between" spacing={5}>
+            {events &&
+              events
+                .filter(event => event.reserved.includes(Meteor.userId()))
+                .map(event => <EventCard key={event._id} event={event} />)}
+          </Grid>
+        )}
+      </div>
+    );
+  }
+}
 
 export default withStyles(styles)(EventGrid);
