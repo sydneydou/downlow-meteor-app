8420cd0e0e412fabc06dc1b42ede0099df6d5026
Sydney Douglas <sydneydouglas1@hotmail.com>
2019-08-08 11:52:49 -0700

    Profile Page styling

---
+++ imports/ui/components/ProfileCard/styles.js
@@ -0,0 +1,10 @@
+const styles = theme => ({
+  profileCard: {
+    paddingTop: "6em",
+    paddingBottom: "3em",
+    paddingLeft: "12em",
+    paddingRight: "12em"
+  }
+});
+
+export default styles;
