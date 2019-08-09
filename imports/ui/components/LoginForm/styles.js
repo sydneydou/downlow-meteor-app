8fdfc0156a3ba28ef8c2544b2530ba89c18c090d
Rosh <rosh_408@hotmail.com>
2019-08-09 13:06:48 -0700

    Conditionally apply className to login input fields when user/pss is incorrect

--- imports/ui/components/LoginForm/styles.js
+++ imports/ui/components/LoginForm/styles.js
@@ -38,8 +38,25 @@ const styles = theme => ({
     width: "300px"
   },
   showErrorColor: {
-    borderColor: "red"
+    backgroundColor: "rgba(0,0,0,0.6)",
+    border: "2px solid red",
+    borderRadius: "10px",
+    width: "90%",
+    color: "white",
+    alignItems: "center",
+    fontSize: "20px",
+    height: "30px",
+    paddingLeft: "5px",
+    paddingRight: "5px",
+    marginBottom: "5px",
+
+    "&::placeholder": {
+      color: "rgba(190, 190, 191, 0.8)",
+      fontSize: "16px",
+      alignSelf: "center",
+      marginTop: "4px"
   }
+}
 });
 
 export default styles;
