e2eb36a67fcc1a00212c8299a5601ce65de38f7b
Rosh <rosh_408@hotmail.com>
2019-08-08 17:03:45 -0700

    Add error message on login form when user/pass is incorrect

--- imports/ui/components/LoginForm/LoginForm.js
+++ imports/ui/components/LoginForm/LoginForm.js
@@ -4,80 +4,98 @@ import Button from "@material-ui/core/Button";
 import { withStyles } from "@material-ui/styles";
 import styles from "./styles";
 
-//
 //TODO: this is the rough code outline for login
 //TODO: add to onSubmit and include toggle between login and register forms
 
-// const passwordHash = require('./lib/password-hash');
-// const hashedPassword = 'sha1$3I7HRwy7$cbfdac6008f9cab4083784cbd1874f76618d2a97';
-
-// console.log(passwordHash.verify('password', hashedPassword)); // true
-// console.log(passwordHash.verify('password', hashedPassword)); // false
+// const bcrypt = require('bcryptjs');
 
 const validate = values => {}; //TODO: Validation
-const onSubmit = values => {
+const onSubmit = (values, handleError) => {
   event.preventDefault();
   var myEmail = values.email;
   var myPassword = values.password;
+  // const hashedPassword = await bcrypt.hash(myPassword, 10);
 
   Meteor.loginWithPassword(myEmail, myPassword, function(error) {
     if (Meteor.user()) {
       console.log(Meteor.userId());
+      handleError(null);
     } else {
-      console.log("ERROR: " + error.reason);
+      console.log("ERROR: " + error);
+      handleError(error);
+      // error.reason ? alert("<div>Sorry! Your User/Password is incorrect! <br/> Please try again! </div>" ) : null
       //return this error message onto screen
     }
   });
 };
 
-const LoginForm = ({ classes }) => (
-  <Form
-    className={classes.Form}
-    onSubmit={values => onSubmit(values)}
-    validate={() => validate()}
-    render={({ handleSubmit, pristine, invalid }) => (
-      <form>
-        <h2 className={classes.LoginTitle}>Login</h2>
+class LoginForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      error: null
+    };
+  }
 
-        <Field
-          name="email"
-          render={({ input, meta }) => (
-            <div>
-              <textarea
-                {...input}
-                placeholder="Email"
-                className={classes["input-field"]}
+  handleError = error => {
+    this.setState({ error });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <React.Fragment>
+        <Form
+          className={classes.Form}
+          onSubmit={values => onSubmit(values, this.handleError)}
+          validate={() => validate()}
+          render={({ handleSubmit, pristine, invalid }) => (
+            <form>
+              <h2 className={classes.LoginTitle}>Login</h2>
+              <Field
+                name="email"
+                render={({ input, meta }) => (
+                  <div>
+                    <textarea
+                      {...input}
+                      placeholder="Email"
+                      className={classes["input-field"]}
+                    />
+                    {meta.touched && meta.error && <span>{meta.error}</span>}
+                  </div>
+                )}
               />
-              {meta.touched && meta.error && <span>{meta.error}</span>}
-            </div>
+              <Field
+                name="password"
+                render={({ input, meta }) => (
+                  <div>
+                    <input
+                      {...input}
+                      type="password"
+                      placeholder="Password"
+                      className={classes["input-field"]}
+                    />
+                    {meta.touched && meta.error && <span>{meta.error}</span>}
+                  </div>
+                )}
+              />
+
+              <Button
+                className={classes.btn}
+                variant="contained"
+                type="submit"
+                disabled={pristine || invalid}
+                onClick={handleSubmit}
+              >
+                Welcome Back!
+              </Button>
+            </form>
           )}
         />
-        <Field
-          name="password"
-          render={({ input, meta }) => (
-            <div>
-              <textarea
-                {...input}
-                placeholder="Password"
-                className={classes["input-field"]}
-              />
-              {meta.touched && meta.error && <span>{meta.error}</span>}
-            </div>
-          )}
-        />
-
-        <Button
-          className={classes.btn}
-          variant="contained"
-          type="submit"
-          disabled={pristine || invalid}
-          onClick={handleSubmit}
-        >
-          Welcome Back!
-        </Button>
-      </form>
-    )}
-  />
-);
+        <p>{this.state.error && this.state.error.reason}</p>
+      </React.Fragment>
+    );
+  }
+}
 
 export default withStyles(styles)(LoginForm);
