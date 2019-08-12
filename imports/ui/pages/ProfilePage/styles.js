dbedf14c1b54a517ed413be15de29e600b511187
Andrew DiBiase <dibiase.andrew@outlook.com>
2019-08-12 16:23:21 -0700

    add DL logo

--- imports/ui/pages/ProfilePage/styles.js
+++ imports/ui/pages/ProfilePage/styles.js
@@ -6,7 +6,8 @@ const styles = theme => ({
     background:
       "linear-gradient(to bottom, #000000 28%,#000000 28%,#000000 30%,#000000 30%,#1c3e73 89%,#1c3e73 91%,#1c3e73 100%,#000000 100%,#7db9e8 100%,#1c3e73 100%,#000000 101%)",
     height: "100%",
-    backgroundSize: "cover"
+    backgroundSize: "cover",
+    paddingBottom: "100px"
   },
   ProfileHeader: {
     color: "white",
